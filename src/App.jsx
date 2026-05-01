ad0d07fdde874842f966f07c48d7ac2524f0d66c
Medina Ali-zade <alizademedina7l@gmail.com>
2026-05-01 21:48:00 +0400

    almost there

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ const isProd = import.meta.env.MODE === 'production';
 
 function App() {
   return (
-<Router basename={isProd ? '/AMmovies' : ''}>      <Navbar />
+<Router basename="/MA-movies">      
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/main" element={<Main type="movie" />} />
