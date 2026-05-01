c23c4e835023413ace178fb39ef4212c7a47dd95
Medina Ali-zade <alizademedina7l@gmail.com>
2026-05-01 21:46:41 +0400

    almost there

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AMmovies/', // This ensures your JS/CSS loads from the subfolder
+  base: '/MA-movies/', // This ensures your JS/CSS loads from the subfolder
 })
